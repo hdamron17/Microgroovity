b3ffd982b5de18171a1ba07bbaea4552136f80dc
JVJohnson <jjohnson18@gssm.k12.sc.us>
2017-02-24 18:51:58 -0500

    Added Doxygen
    Put Doxygen on the functions correctly this time I think

--- src/integrator.py
+++ src/integrator.py
@@ -4,22 +4,43 @@ Generic numeric integrations for 2017 microgravity project
 
 
 def egg_function(y):
+    """
+    Creates an egg with the desired parameters
+    
+    :param y: the desired Y value of the egg function
+    """
     #TODO figure out what other parameters are required for egg function
     pass #TODO
 
 def groove_function(y):
+    """
+    Creates a Groove for an egg with the desired parameters
+    
+    :param y: the Y value of the groove
+    """
     #TODO figure out what other parameters
     pass #TODO
 
 def perimeter(y):
+    """
+    Calculates the perimeter of an egg on a cross-section
+    
+    :param y: the Y value of the desired cross-section
+    """
     pass #TODO
 
 def area(y):
+    """
+    Calculates the frontal area of an egg
+    
+    :param y: the Y value of a desired egg for calculation
+    """
     pass #TODO
 
 def accel(y, vel, Cd):
     """ 
     Calculates acceleration on a specific egg
+    
     :param y: y position of egg
     :param vel: y velocity of egg
     :param Cd: drag coefficient
