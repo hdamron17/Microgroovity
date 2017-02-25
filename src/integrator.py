97ffe169752eb1e6ede33e83298ab02485ebf6fa
Tesla Waters <twaters17@gssm.k12.sc.us>
2017-02-24 22:58:59 -0500

    A few pieces of the implementation.

--- src/integrator.py
+++ src/integrator.py
@@ -2,7 +2,10 @@
 """ 
 Generic numeric integrations for 2017 microgravity project
 """
+import math
 
+
+max_y = 1 - 1/math.sqrt(3)
 
 def egg_function(y, A):
     """
@@ -12,7 +15,7 @@ def egg_function(y, A):
     :param A: coefficient which increases width of egg
     """
     #TODO figure out what other parameters are required for egg function
-    pass #TODO
+    return math.sqrt(y * (1 - y) * (1 - y/2))
 
 def groove_function(y):
     """
@@ -25,7 +28,17 @@ def groove_function(y):
     #TODO figure out what other parameters
     pass #TODO
 
-def perimeter(y, groove_angle, n):
+def egg_derivative(y, A):
+    """
+    Calculates derivative of egg function at position y
+    
+    :param y: y position for which to find egg prime
+    :param A: A width coefficient for egg function
+    :return: Returns derivative of egg function at position y
+    """
+    return A / (2 * egg_function(y, A)) * (3/2 * y**2 - 3 * y + 1)
+
+def perimeter(y, groove_angle, n, A):
     """
     Calculates the perimeter of an egg on a cross-section
     
@@ -34,7 +47,14 @@ def perimeter(y, groove_angle, n):
     :param n: number of grooves
     :return: Returns perimeter of egg intersection with plane at position y
     """
-    pass #TODO
+    assert max_y <= y <= 1, "y out of range" #checks range (does not include perimeter below max point)
+    w = egg_function(y, A) * 2 #egg width
+    d = w / 2 - groove_function(y) #depth = w/2 - groove function
+    #separated into parts for typing simplicity
+    #derivation by Gracen
+    part1 = n * w / 2 * (math.tau / n - groove_angle)
+    part2 = 2*n * math.sqrt(d**2 + 1/8 * w**2 * (1-math.cos(groove_angle)))
+    return part1 + part2
 
 def area(y):
     """
@@ -42,7 +62,7 @@ def area(y):
     
     :param y: the Y value of a desired egg for calculation
     """
-    pass #TODO
+    
 
 def accel(y, vel, Cd):
     """ 
@@ -56,3 +76,9 @@ def accel(y, vel, Cd):
     """
     water_density = 1 #TODO density of water in correct units
     drag_force = 0.5 * Cd * area * water_density * vel**2
+
+def depth():
+    y0 = max_y
+
+if __name__ == "__main__":
+    print("TODO")
